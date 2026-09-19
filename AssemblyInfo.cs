ee21a1b25535eead521e37d80edc02b82d74c6c6
volong99-stack <vo.long99@gmail.com>
2026-09-19 07:13:37 -0500

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,11 +3,11 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("TRON Direct Enforcement")]
-[assembly: AssemblyDescription("Explicit administrator-controller firewall adapter; private release candidate")]
+[assembly: AssemblyDescription("Explicit administrator-controller firewall adapter; unsigned developer preview")]
 [assembly: AssemblyProduct("TRON Direct Enforcement")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("")]
-[assembly: AssemblyConfiguration("Private Release Candidate")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-private-candidate")]
+[assembly: AssemblyConfiguration("Unsigned Developer Preview")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
+[assembly: AssemblyInformationalVersion("0.1.0-dev-preview.1")]
